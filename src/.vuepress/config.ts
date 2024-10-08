898f533da031c616c7333c579771e273869eabd5
mishu <riceuncle@outlook.com>
2024-10-08 11:52:14 +0800

    docsearch

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -28,6 +28,43 @@ export default defineUserConfig({
             button: {
               buttonText: '搜索文档',
             },
+            modal: {
+              searchBox: {
+                resetButtonTitle: '清空查询',
+                resetButtonAriaLabel: '清空查询',
+                cancelButtonText: '返回',
+                cancelButtonAriaLabel: '返回',
+                searchInputLabel: '搜索',
+              },
+              startScreen: {
+                recentSearchesTitle: '最近',
+                noRecentSearchesText: '没有最近的搜索',
+                saveRecentSearchButtonTitle: '保存此搜索',
+                removeRecentSearchButtonTitle: '从历史记录中删除此搜索',
+                favoriteSearchesTitle: '收藏',
+                removeFavoriteSearchButtonTitle: '从收藏夹中删除此搜索',
+              },
+              errorScreen: {
+                titleText: '无法获取结果',
+                helpText: '您可能需要检查您的网络连接。',
+              },
+              footer: {
+                selectText: '选择',
+                selectKeyAriaLabel: 'Enter key',
+                navigateText: '切换',
+                navigateUpKeyAriaLabel: 'Arrow up',
+                navigateDownKeyAriaLabel: 'Arrow down',
+                closeText: '关闭',
+                closeKeyAriaLabel: 'Escape key',
+                searchByText: 'Search by',
+              },
+              noResultsScreen: {
+                noResultsText: '没有结果',
+                suggestedQueryText: '尝试搜索',
+                reportMissingResultsText: '相信这个查询应该返回结果吗？',
+                reportMissingResultsLinkText: '让我们知道。',
+              },
+            },
           },
         },
       }
